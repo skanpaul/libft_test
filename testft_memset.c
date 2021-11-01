07b911bb1fee1e3fd9fde81263ba129a31601b27
sorakann <sorakann.ki@gmail.com>
2021-11-01 08:00:47 +0100

    autre

--- testft_memset.c
+++ testft_memset.c
@@ -1,20 +1,49 @@
+#include "../libft/libft.h"
 #include "libft_test.h"
 #include <string.h>
 #include <stdio.h>
 
 void testft_memset(void)
 {
-	int length = 5;
-	int c = 64;
-
+	int length;
+	int c;	
+	char text[20];
 	void *ptr;
-
-	char text[20] = "le soleil";
-	printf("pointer: %p | text: %s\n\n", text, text);
-
-	ptr = memset(text, c, length);
-
-	printf("pointer: %p | text: %s\n\n", ptr, text);
-
 	
+	c = 64;
+	length = 0;
+	strcpy(text, "le soleil");
+	printf("   given: byte %o | len: %d | pointer: %p | text: %s\n", c, length, ptr, text);
+	ptr = memset(text, c, length);
+	printf("official: byte %o | len: %d | pointer: %p | text: %s\n", c, length, ptr, text);
+	ptr = ft_memset(text, c, length);
+	printf("  result: byte %o | len: %d | pointer: %p | text: %s\n\n", c, length, ptr, text);
+
+	c = 64;
+	length = 5;
+	strcpy(text, "le soleil");
+	printf("   given: byte %o | len: %d | pointer: %p | text: %s\n", c, length, ptr, text);
+	ptr = memset(text, c, length);
+	printf("official: byte %o | len: %d | pointer: %p | text: %s\n", c, length, ptr, text);
+	ptr = ft_memset(text, c, length);
+	printf("  result: byte %o | len: %d | pointer: %p | text: %s\n\n", c, length, ptr, text);
+
+	c = 64;
+	length = 20;
+	strcpy(text, "le soleil");
+	printf("   given: byte %o | len: %d | pointer: %p | text: %s\n", c, length, ptr, text);
+	ptr = memset(text, c, length);
+	printf("official: byte %o | len: %d | pointer: %p | text: %s\n", c, length, ptr, text);
+	ptr = ft_memset(text, c, length);
+	printf("  result: byte %o | len: %d | pointer: %p | text: %s\n\n", c, length, ptr, text);
+
+	c = 64;
+	length = 21;
+	strcpy(text, "le soleil");
+	printf("   given: byte %o | len: %d | pointer: %p | text: %s\n", c, length, ptr, text);
+	ptr = memset(text, c, length);
+	printf("official: byte %o | len: %d | pointer: %p | text: %s\n", c, length, ptr, text);
+	ptr = ft_memset(text, c, length);
+	printf("  result: byte %o | len: %d | pointer: %p | text: %s\n\n", c, length, ptr, text);
+
 }
