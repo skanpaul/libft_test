db458e96f36cf9b0c027c2a2623158cf570f584b
sorakann <sorakann.ki@gmail.com>
2021-11-23 22:29:55 +0100

    Work From Home

--- testft_bzero.c
+++ testft_bzero.c
@@ -7,42 +7,42 @@
 
 void testft_bzero(void)
 {
-	size_t n;
-	char data_1[10];
-	char data_2[10];
+	// size_t n;
+	// char data_1[10];
+	// char data_2[10];
 	
-    // TEST 01: -----------------------------------
-	strcpy(data_1, "abcdefghi");
-	strcpy(data_2, "abcdefghi");
-	n = 1;
-	ft_bzero(data_1, n);
-	bzero(data_2, n);
+    // // TEST 01: -----------------------------------
+	// strcpy(data_1, "abcdefghi");
+	// strcpy(data_2, "abcdefghi");
+	// n = 1;
+	// ft_bzero(data_1, n);
+	// bzero(data_2, n);
 
-	// TEST 02: -----------------------------------
-	strcpy(data_1, "abcdefghi");
-	strcpy(data_2, "abcdefghi");
-	n = 1;
-	ft_bzero(data_1, n);
-	bzero(data_2, n);
+	// // TEST 02: -----------------------------------
+	// strcpy(data_1, "abcdefghi");
+	// strcpy(data_2, "abcdefghi");
+	// n = 1;
+	// ft_bzero(data_1, n);
+	// bzero(data_2, n);
 
-	// TEST 03: -----------------------------------
-	strcpy(data_1, "abcdefghi");
-	strcpy(data_2, "abcdefghi");
-	n = 9;
-	ft_bzero(data_1, n);
-	bzero(data_2, n);
+	// // TEST 03: -----------------------------------
+	// strcpy(data_1, "abcdefghi");
+	// strcpy(data_2, "abcdefghi");
+	// n = 9;
+	// ft_bzero(data_1, n);
+	// bzero(data_2, n);
 
-	// TEST 04: -----------------------------------
-	strcpy(data_1, "abcdefghi");
-	strcpy(data_2, "abcdefghi");
-	n = 10;
-	ft_bzero(data_1, n);
-	bzero(data_2, n);
+	// // TEST 04: -----------------------------------
+	// strcpy(data_1, "abcdefghi");
+	// strcpy(data_2, "abcdefghi");
+	// n = 10;
+	// ft_bzero(data_1, n);
+	// bzero(data_2, n);
 
-	// TEST 05: -----------------------------------
-	strcpy(data_1, "abcdefghi");
-	strcpy(data_2, "abcdefghi");
-	n = 11;
-	ft_bzero(data_1, n);
-	bzero(data_2, n);
+	// // TEST 05: -----------------------------------
+	// strcpy(data_1, "abcdefghi");
+	// strcpy(data_2, "abcdefghi");
+	// n = 11;
+	// ft_bzero(data_1, n);
+	// bzero(data_2, n);
 }
--- testft_memcpy.c
+++ testft_memcpy.c
@@ -6,36 +6,36 @@
 /* ************************************************************************** */
 void testft_memcpy(void)
 {
-	size_t n;
-	char source[15];
-	char destination[15];	
+	// size_t n;
+	// char source[15];
+	// char destination[15];	
 
-	strcpy(source, "il fait beau");
-	// -------------------------------------
-	n = 0;
-	ft_memcpy(destination, source, n);
-	memcpy(destination, source, n);
-	// -------------------------------------
-	n = 5;
-	ft_memcpy(destination, source, n);
-	memcpy(destination, source, n);
-	// -------------------------------------
-	n = 14;
-	ft_memcpy(destination, source, n);
-	memcpy(destination, source, n);
-	// -------------------------------------
-	// n = 15;
+	// strcpy(source, "il fait beau");
+	// // -------------------------------------
+	// n = 0;
 	// ft_memcpy(destination, source, n);
 	// memcpy(destination, source, n);
 	// // -------------------------------------
-	// n = 16;
+	// n = 5;
 	// ft_memcpy(destination, source, n);
 	// memcpy(destination, source, n);
+	// // -------------------------------------
+	// n = 14;
+	// ft_memcpy(destination, source, n);
+	// memcpy(destination, source, n);
+	// // -------------------------------------
+	// // n = 15;
+	// // ft_memcpy(destination, source, n);
+	// // memcpy(destination, source, n);
+	// // // -------------------------------------
+	// // n = 16;
+	// // ft_memcpy(destination, source, n);
+	// // memcpy(destination, source, n);
 
-	// ft_memcpy(((void *)0), ((void *)0), 3);
-	n = 3;
-	ft_memcpy(((void *)0), ((void *)0), n);
-	memcpy(((void *)0), ((void *)0), n);
+	// // ft_memcpy(((void *)0), ((void *)0), 3);
+	// n = 3;
+	// ft_memcpy(((void *)0), ((void *)0), n);
+	// memcpy(((void *)0), ((void *)0), n);
 
 
 
