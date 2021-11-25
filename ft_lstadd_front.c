44afe5f5a96440a3c67113c4c179bc6c076be0fb
sorakann iMac <ski@student.42lausanne.com>
2021-11-25 08:42:55 +0100

    create ft_lstadd_front from scratch

--- ft_lstadd_front.c
+++ ft_lstadd_front.c
@@ -14,30 +14,14 @@
 /* ************************************************************************** */
 void	ft_lstadd_front(t_list **alst, t_list *new)
 {
-	if ((alst != NULL) && (new != NULL))
-	{
-		new->next = *alst;
+	if (alst == NULL)
+		return ;
+	if (*alst == NULL)
 		*alst = new;
+	else
+	{
+		(*new).next = (*alst);
+		(*alst) = new;
 	}
+	return ;
 }
-
-// void	ft_lstadd_front_BBB(t_list **alst, t_list *new)
-// {
-// 	size_t i;
-
-// 	if(!alst)
-// 		return ;
-	
-// 	i = 0;	
-// 	while (alst[i] != NULL)
-// 		i++;
-
-// 	while(i > 0)
-// 	{
-// 		alst[i + 1] = 	alst[i];
-// 		i--;
-// 	}
-// 	alst[i + 1] = 	alst[i];
-// 	alst[i] = new;
-// 	new->next = alst[1];
-// }
