e2e5954bec1d37ead5cc84a4facb71425ae41802
sorakann iMac <ski@student.42lausanne.com>
2021-11-25 14:02:36 +0100

    ft_lstiter DONE

--- ft_lstiter.c
+++ ft_lstiter.c
@@ -9,10 +9,23 @@
 /*   Updated: 2021/11/25 13:49:19 by ski              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include "libft_test.h"
+#include "libft.h"
 
 /* ************************************************************************** */
 void	ft_lstiter(t_list *lst, void (*f)(void *))
 {
+	t_list	*ptr1;
+	t_list	*ptr2;
 
+	ptr1 = lst;
+	ptr2 = NULL;
+	if ((ptr1 == NULL) || (f == NULL))
+		return ;
+	while (ptr1->next != NULL)
+	{
+		ptr2 = ptr1->next;
+		f(ptr1->content);
+		ptr1 = ptr2;
+	}
+	f(ptr1->content);
 }
